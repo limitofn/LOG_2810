a4c91b998c3b4c97d60e73586b85e0a94ef70317
pastank <54858349+pastank@users.noreply.github.com>
2019-11-04 23:04:50 +0100

    pb avec import commande

--- TP1/djikstra_algo.py
+++ TP1/djikstra_algo.py
@@ -1,19 +1,21 @@
 import networkx as nx
-import commande as commande
+import Commande as commande
 
-def graph_to_length (graph, source_node) :
+def graph_to_length (graph, source_node):
     length, path = nx.single_source_dijkstra(graph, source_node)
     return length, path
 
-def path_to_object (graph, path, cmd) :
-    node
+def path_to_object (graph, path, commandePasse):
     panier = commande.command
-    for option in path :
-        objectList = [0, 0, 0]
-        for noeud in option :
-            objectList += graph[noeud].A
-            objectList += graph[noeud].B
-            objectList += graph[noeud].C
-            listeDeListe += objectList
-    return node
+    for option in path:
+        nbreA = 0
+        nbreB = 0
+        nbreC = 0
+        for noeud in option:
+            nbreA += graph.node['noeud']['nbA']
+            nbreB += graph.node['noeud']['nbB']
+            nbreC += graph.node['noeud']['nbC']
+            if (nbreA >= panier.nbA) & (nbreB >= panier.nbB) & (nbreC >= panier.nbC):
+                return path[option]
+
 
