9ed68d8afddfcfbb657bd8a06f06a874197f37cf
pastank <54858349+pastank@users.noreply.github.com>
2019-11-05 00:03:40 +0100

    lecture dico ok

--- TP1/djikstra_algo.py
+++ TP1/djikstra_algo.py
@@ -1,21 +1,21 @@
 import networkx as nx
-import Commande as commande
+
 
 def graph_to_length (graph, source_node):
     length, path = nx.single_source_dijkstra(graph, source_node)
     return length, path
 
+
 def path_to_object (graph, path, commandePasse):
-    panier = commande.command
-    for option in path:
+    for key in path.keys():
         nbreA = 0
         nbreB = 0
         nbreC = 0
-        for noeud in option:
-            nbreA += graph.node['noeud']['nbA']
-            nbreB += graph.node['noeud']['nbB']
-            nbreC += graph.node['noeud']['nbC']
-            if (nbreA >= panier.nbA) & (nbreB >= panier.nbB) & (nbreC >= panier.nbC):
+        for noeud in path[key]:
+            nbreA += graph.node[noeud]['nbA']
+            nbreB += graph.node[noeud]['nbB']
+            nbreC += graph.node[noeud]['nbC']
+            if (nbreA >= commandePasse.nbA) & (nbreB >= commandePasse.nbB) & (nbreC >= commandePasse.nbC):
                 return path[option]
 
 
