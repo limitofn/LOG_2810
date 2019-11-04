fe311c6710d523e076ec7bcb5e303fba1c4b7e00
pastank <54858349+pastank@users.noreply.github.com>
2019-11-04 23:51:12 +0100

    Merge branch 'master' of /home/satan/PycharmProjects/LOG_2810 with conflicts.

--- TP1/djikstra_algo.py
+++ TP1/djikstra_algo.py
@@ -6,15 +6,16 @@ def graph_to_length (graph, source_node):
     return length, path
 
 def path_to_object (graph, path, commandePasse):
-    print(path)
-    for list in path:
+    panier = commande.command
+    for option in path:
         nbreA = 0
         nbreB = 0
         nbreC = 0
-        nbreA += graph.node[list]['nbA']
-        nbreB += graph.node[list]['nbB']
-        nbreC += graph.node[list]['nbC']
-        if (nbreA >= commandePasse.nbA) & (nbreB >= commandePasse.nbB) & (nbreC >= commandePasse.nbC):
-            return path[list]
+        for noeud in option:
+            nbreA += graph.node['noeud']['nbA']
+            nbreB += graph.node['noeud']['nbB']
+            nbreC += graph.node['noeud']['nbC']
+            if (nbreA >= panier.nbA) & (nbreB >= panier.nbB) & (nbreC >= panier.nbC):
+                return path[option]
 
 
