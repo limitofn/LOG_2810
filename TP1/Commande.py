1400a31d2ed89fb90c7bdf7707117ed8d0cc50fd
Unknown <marcandreditlebreton@hotmail.com>
2019-11-04 13:11:37 -0500

    Selection du robot dans la classe commande
    Retourne un string

--- TP1/Commande.py
+++ TP1/Commande.py
@@ -7,4 +7,20 @@ class Command:
         self.nbA = int(input('Nombre objet A'))
         self.nbB = int(input('Nombre objet B'))
         self.nbC = int(input('Nombre objet C'))
-        
+
+    def commandeValide(self):
+        #Verifie si le poid est valide et retourne le meilleur robot
+        poid = 1*self.nbA + 3*self.nbB + 6*self.nbC
+        if( poid > 25):
+            print('Charge Impossible')
+            return None
+        elif(10 < poid <= 25):
+            print('Utilisation robot Z')
+            return 'Z'
+        elif (5 < poid <= 10):
+            print('Utilisation robot Y')
+            return 'Y'
+        elif (0 < poid <= 5):
+            print('Utilisation robot X')
+            return 'X'
+
