d84df614b069f34bc126dd57ad476d95cdff247b
Unknown <marcandreditlebreton@hotmail.com>
2019-11-04 20:41:58 -0500

    algo marche

--- TP1/djikstra_algo.py
+++ TP1/djikstra_algo.py
@@ -12,10 +12,10 @@ def path_to_object (graph, path, commandePasse):
         nbreB = 0
         nbreC = 0
         for noeud in path[key]:
-            nbreA += graph.node[noeud]['nbA']
-            nbreB += graph.node[noeud]['nbB']
-            nbreC += graph.node[noeud]['nbC']
+            nbreA += graph._node[noeud]['nbA']
+            nbreB += graph._node[noeud]['nbB']
+            nbreC += graph._node[noeud]['nbC']
             if (nbreA >= commandePasse.nbA) & (nbreB >= commandePasse.nbB) & (nbreC >= commandePasse.nbC):
-                return path[option]
+                return path[noeud]
 
 
