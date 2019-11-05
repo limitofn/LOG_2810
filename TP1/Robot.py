05cf3712e718cf907bac51fde970cf91040ef312
Unknown <marcandreditlebreton@hotmail.com>
2019-11-04 21:23:28 -0500

    Parcourschemin

--- TP1/Robot.py
+++ TP1/Robot.py
@@ -1,13 +1,26 @@
-class Robot:
-    def __init__(self, type):
-        conteneurA = 0
-        conteneurB = 0
-        conteneurC = 0
-        type = type
+        class Robot:
+            def __init__(self, type):
+                conteneurA = 0
+                conteneurB = 0
+                conteneurC = 0
+                type = type
+                CONST_K = 0
 
-    def setter(self, nbA,nbB,nbC):
-        self.conteneurA = nbA
-        self.conteneurB = nbB
-        self.conteneurC = nbC
+            def setter(self, nbA, nbB, nbC):
+                self.conteneurA = nbA
+                self.conteneurB = nbB
+                self.conteneurC = nbC
 
-    def
+            def parcoursChemin(self, path, command, graph):
+                #On prends le chemin a parcourir de node debut a node end.
+                #Calcul du cout vers la node de fin
+                cout = 0
+                
+                previous = None
+                for index in path:
+                    if(previous != None):
+                        cout += graph[previous][index]['weight']
+
+
+
+
