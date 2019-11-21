46d33235cb405f4ed0c96b2b8d3abc3339b4076c
Guillaume Proulx <guillaume_proulx@outlook.com>
2019-11-21 10:49:33 -0500

    ajout de la classe order pour la commande

---
+++ TP2/Order.py
@@ -0,0 +1,52 @@
+class Order:
+    def __init__(self):
+        self.order = []
+        self.weight = 0
+
+    def addObject(self, listObject, object):
+        self.order.append(object)
+
+        if object.type == 'A':
+            self.weight += 1
+        if object.type == 'B':
+            self.weight += 3
+        if object.type == 'C':
+            self.weight += 5
+
+        for i in listObject:
+            if i == object:
+                listObject.remove(i)
+        return listObject
+
+    def printOrder(self):
+        if self.order == None:
+            print("Désolé, votre panier est vide")
+        else:
+            print("Votre panier contient:")
+            for i in self.order:
+                print(self.order[i])
+            print("Et le poid du panier est de " + str(self.weight) + "Kg")
+
+    def removeAllObject(self, listObject):
+        for object in self.order:
+            listObject.append(object)
+        self.order = []
+        return listObject
+
+    def checkOut(self, listObject):
+        for object in self.order:
+            if object.type == 'A':
+                self.weight += 1
+            if object.type == 'B':
+                self.weight += 3
+            if object.type == 'C':
+                self.weight += 5
+        if self.weight >= 25:
+            newInventory = self.removeAllObject(listObject)
+            print("Panier trop lourd! Commande abandonnnee")
+            return newInventory
+        else:
+            print("Commande passee! Merci d'avoir magasine chez nous!")
+            self.order = []
+
+
